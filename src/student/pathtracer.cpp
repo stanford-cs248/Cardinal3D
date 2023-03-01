4f005c6ff876740f6369f846e81cb6e1a9c6b09c
Magatha Wang <magatha.wang@gmail.com>
2023-02-28 19:08:42 -0500

    addd offset and sampler size

--- src/student/pathtracer.cpp
+++ src/student/pathtracer.cpp
@@ -27,7 +27,7 @@ Spectrum Pathtracer::trace_pixel(size_t x, size_t y) {
         // If n_samples > 1, please send the ray through any random point within the pixel
         Samplers::Rect::Uniform rect_sampler(Vec2(1.f, 1.f));
         float pdf;
-        Vec2 rand_point = rect_sampler.sample(pdf);
+        Vec2 rand_point = rect_sampler.sample(pdf); // (0,0) -> sample(0,0) = (0.1, 0.1) -> generate_ray(0.1,0.1) | (h,w) = (10,10) 
         out = camera.generate_ray((xy + rand_point) / wh);
     } else {
 
@@ -39,7 +39,12 @@ Spectrum Pathtracer::trace_pixel(size_t x, size_t y) {
     // Tip: you may want to use log_ray for debugging. Given ray t, the following lines
     // of code will log .03% of all rays (see util/rand.h) for visualization in the app.
     // see student/debug.h for more detail.
-    if(RNG::coin_flip(0.0005f)) log_ray(out, 10.0f);
+    // if(RNG::coin_flip(0.0005f)) log_ray(out, 10.0f);
+    log_ray(camera.generate_ray(Vec2(.5f,.5f) / wh), 10.0f);
+    log_ray(camera.generate_ray(Vec2(0.f,0.f) / wh), 10.0f);
+    log_ray(camera.generate_ray(Vec2(1.f,1.f) / wh), 10.0f);
+    log_ray(camera.generate_ray(Vec2(1.f,0.f) / wh), 10.0f);
+    log_ray(camera.generate_ray(Vec2(0.f,1.f) / wh), 10.0f);
 
     return trace_ray(out);
 }
