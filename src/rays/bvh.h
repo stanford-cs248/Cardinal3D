e67e4fdd06aa824a069c2a8f618052b224f8ffae
avasisht23 <ajay@alchemy.com>
2023-03-02 22:43:57 -0500

    fix: refactor

--- src/rays/bvh.h
+++ src/rays/bvh.h
@@ -44,10 +44,6 @@ private:
         size_t primitives_count = 0;
     };
     void build_helper(int node_idx, size_t max_leaf_size);
-    bool get_lowest_cost_SAH(float node_surface_area, float bucket_space_start, float bucket_size,
-                             const std::vector<BVH<Primitive>::Bucket>& buckets, float& lowest_cost,
-                             float& pivot);
-
     void hit_helper(const Ray& ray, const Node& node, Trace* ret) const;
 
     std::vector<Node> nodes;
