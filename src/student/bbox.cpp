14f84117674869f53228ff1f63429a94d54df033
avasisht23 <ajay@alchemy.com>
2023-03-02 23:03:47 -0500

    swap t_min and t_max

--- src/student/bbox.cpp
+++ src/student/bbox.cpp
@@ -26,8 +26,8 @@ bool BBox::hit(const Ray& ray, Vec2& times) const {
         std::swap(isect_min.z, isect_max.z);
     }
 
-    float t_min = std::min(std::min(isect_max.x, isect_max.y), isect_max.z);
-    float t_max = std::max(std::max(isect_min.x, isect_min.y), isect_min.z);
+    float t_min = std::max(std::max(isect_min.x, isect_min.y), isect_min.z);
+    float t_max = std::min(std::min(isect_max.x, isect_max.y), isect_max.z);
 
     if(t_min > t_max) {
         return false;
